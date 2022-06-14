54952d1ce5e94ec32211455d63db06ea4a46e2e4
Luc Dumas <62025065+lucdumas@users.noreply.github.com>
2022-06-14 09:57:27 -0400

    Sum the strings as a number

---
+++ sumStringsAsNums.js
@@ -0,0 +1,9 @@
+// Given the string representations of two integers, return the string representation of the sum of those integers.
+
+
+function sumStrings(a,b) { 
+ let aToNum= Number(a)
+  let bToNum=Number(b)
+  
+ return (aToNum + bToNum).toString()
+}
