5b229598e444da834caace75e71669f48eb69abb
Luc Dumas <62025065+lucdumas@users.noreply.github.com>
2022-07-22 14:11:30 -0400

    Figure out how many pies we all get!

---
+++ esperanzasPies.js
@@ -0,0 +1,13 @@
+// Story
+// My seven brothers and I asked miss Esperanza to bake t cakes. Luckily she likes to cook, so she decided to bake ten times more cakes than we asked for. However, we can't eat that many, so we decided to split them equally between all of us and give the rest back to her.
+
+// Task
+// Please calculate how many cakes will each of us get.
+
+// Code Limit
+// Less than 25 characters.
+
+
+
+
+esperanzasPies=t=>t/.8|0
